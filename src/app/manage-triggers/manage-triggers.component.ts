a3235a07b45c2ce2d44fb9e0b35b7dec6224fa94
Carlos D Ordonez <carlos@carlosdaniel.info>
2024-03-05 19:53:21 -0700

    Adding Triggers show ups

--- src/app/manage-triggers/manage-triggers.component.ts
+++ src/app/manage-triggers/manage-triggers.component.ts
@@ -1,12 +1,41 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Trigger } from '../trigger';
 
 @Component({
   selector: 'app-manage-triggers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './manage-triggers.component.html',
   styleUrl: './manage-triggers.component.scss'
 })
 export class ManageTriggersComponent {
 
+  triggerList: Trigger = {
+    cooldown: 0,
+    cost: 500,
+    date: new Date(),
+    _id: '5164asdas231das4fdasf',
+    file: 'Chimuelo.mp3',
+    mediaType: 'video/mp4',
+    name: 'Chimuelo Meme',
+    type: 'redemption'
+  };
+
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
+  ngOnInit() {
+    this.getTriggerList();
+  }
+
+  getTriggerList() {
+    // this.http.get('https://api.domdimabot.com/triggers/cdom201').subscribe((response: any) => {
+    //   this.triggerList = response;
+    //   console.log(this.triggerList);
+    // });
+  }
 }
---
+++ src/app/trigger.ts
@@ -0,0 +1,10 @@
+export interface Trigger {
+    cooldown: number;
+    cost: number;
+    date: Object;
+    _id: string;
+    file: string;
+    mediaType: string;
+    name: string;
+    type: string;
+}
