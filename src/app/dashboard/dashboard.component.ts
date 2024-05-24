f538e35bb98a05a6b72649cbf681bfa653cda4d9
CDOM201 <carlos@carlosdaniel.info>
2024-05-23 21:33:38 -0700

    Updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { PageAlertsComponent } from '../page-alerts/page-alerts.component';
 import { UserService } from '../user.service';
 import { LinksService } from '../links.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { AlertsService } from '../alerts.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -24,7 +24,8 @@ export class DashboardComponent {
     private userService: UserService,
     private linksService: LinksService,
     private alertsService: AlertsService,
-    private matSnackBar: MatSnackBar
+    private matSnackBar: MatSnackBar,
+    private location: Location
   ) { }
 
   ngOnInit() {
@@ -32,6 +33,17 @@ export class DashboardComponent {
       this.joinable = false;
     }
     this.authURL = `https://id.twitch.tv/oauth2/authorize?response_type=code&force_verify=false&client_id=jl9k3mi67pmrbl1bh67y07ezjdc4cf&redirect_uri=${this.linksService.getApiURL()}/auth&scope=analytics%3Aread%3Aextensions+analytics%3Aread%3Agames+bits%3Aread+channel%3Amanage%3Aads+channel%3Aread%3Aads+channel%3Amanage%3Abroadcast+channel%3Aread%3Acharity+channel%3Aedit%3Acommercial+channel%3Aread%3Aeditors+channel%3Amanage%3Aextensions+channel%3Aread%3Agoals+channel%3Aread%3Aguest_star+channel%3Amanage%3Aguest_star+channel%3Aread%3Ahype_train+channel%3Amanage%3Amoderators+channel%3Aread%3Apolls+channel%3Amanage%3Apolls+channel%3Aread%3Apredictions+channel%3Amanage%3Apredictions+channel%3Amanage%3Araids+channel%3Aread%3Aredemptions+channel%3Amanage%3Aredemptions+channel%3Amanage%3Aschedule+channel%3Aread%3Astream_key+channel%3Aread%3Asubscriptions+channel%3Amanage%3Avideos+channel%3Aread%3Avips+channel%3Amanage%3Avips+clips%3Aedit+moderation%3Aread+moderator%3Amanage%3Aannouncements+moderator%3Amanage%3Aautomod+moderator%3Aread%3Aautomod_settings+moderator%3Amanage%3Aautomod_settings+moderator%3Amanage%3Abanned_users+moderator%3Aread%3Ablocked_terms+moderator%3Amanage%3Ablocked_terms+moderator%3Amanage%3Achat_messages+moderator%3Aread%3Achat_settings+moderator%3Amanage%3Achat_settings+moderator%3Aread%3Achatters+moderator%3Aread%3Afollowers+moderator%3Aread%3Aguest_star+moderator%3Amanage%3Aguest_star+moderator%3Aread%3Ashield_mode+moderator%3Amanage%3Ashield_mode+moderator%3Aread%3Ashoutouts+moderator%3Amanage%3Ashoutouts+user%3Aedit+user%3Aedit%3Afollows+user%3Aread%3Ablocked_users+user%3Amanage%3Ablocked_users+user%3Aread%3Abroadcast+user%3Amanage%3Achat_color+user%3Aread%3Aemail+user%3Aread%3Afollows+user%3Aread%3Asubscriptions+user%3Amanage%3Awhispers+channel%3Abot+channel%3Amoderate+chat%3Aedit+chat%3Aread+user%3Abot+user%3Aread%3Achat+whispers%3Aread+whispers%3Aedit&state=${this.userService.getUsername()}`
+
+    let url = this.location.path();
+    let params = url.split('?')[1];
+    if (params) {
+      let alert = params.split('=')[1];
+      if(alert) {
+        alert = decodeURIComponent(alert);
+        this.matSnackBar.open(alert, 'Dismiss', { duration: 2500 });
+      }
+    }
+    
   }
 
   givePermissions() {
--- src/app/song-request-module/song-request-module.component.ts
+++ src/app/song-request-module/song-request-module.component.ts
@@ -21,6 +21,21 @@ import { MatCardModule } from '@angular/material/card';
 export class SongRequestModuleComponent {
   showForm: boolean = false;
   cardAvailable: boolean = false;
+  songReward: any = {
+    _id: '',
+    title: '',
+    prompt: '',
+    cost: 0,
+    skipQueue: false,
+    priceIncrease: 0,
+    returnToOriginalCost: false,
+    rewardMessage: '',
+    rewardType: '',
+    cooldown: 0,
+    rewardID: ''
+  }
+
+  editForm: boolean = false;
 
   songRequestForm = this.fb.group({
     _id: [''],
@@ -42,33 +57,58 @@ export class SongRequestModuleComponent {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    let reward = await this.rewardService.getCustomRewards();
+    let reward = await this.rewardService.getSongReward();
+    reward.length > 0 ? this.cardAvailable = true : this.cardAvailable = false;
+    this.songReward = reward[0];
   }
 
   showFormToggle(): void {
     this.showForm = !this.showForm;
+
+    if(!this.showForm) {
+      this.editForm = false;
+      this.songRequestForm.reset();
+    }
   }
 
+  startEditForm() {
+    this.songRequestForm.patchValue({
+      _id: this.songReward._id,
+      title: this.songReward.rewardTitle,
+      prompt: this.songReward.rewardPrompt,
+      cost: this.songReward.rewardCost,
+      skipQueue: this.songReward.skipQueue ?? false,
+      priceIncrease: this.songReward.rewardCostChange > 0 ? true : false,
+      priceIncreaseAmount: this.songReward.rewardCostChange,
+      returnToOriginalPrice: this.songReward.returnToOriginalCost ?? false,
+      message: this.songReward.rewardMessage,
+      cooldown: this.songReward.cooldown,
+      rewardType: this.songReward.rewardType
+    });
+
+    this.editForm = true;
+    this.showFormToggle();
+  }
+  
   async songRequestSubmit(): Promise<void> {
     if (this.songRequestForm.valid) {
-      let rewardData: Reward = {
+      let rewardData: any = {
         title: this.title!.value ?? 'Song Request',
         prompt: this.prompt!.value ?? '',
         cost: this.cost!.value ?? 1,
         skipQueue: this.skipQueue!.value ?? false,
         priceIncrease: this.priceIncreaseAmount!.value ?? 0,
         returnToOriginalCost: this.returnToOriginalPrice!.value ?? false,
-        rewardMessage: this.message!.value ?? '',
+        message: this.message!.value ?? '',
         rewardType: this.rewardType!.value ?? 'song',
-        cooldown: this.cooldown!.value ?? 0
+        cooldown: this.cooldown!.value ?? 0,
+        userInput: true
       }
       
       if(this.cooldown!.value ?? 0 > 0) {
         rewardData.global_cooldown_seconds = this.cooldown!.value ?? 0;
         rewardData.is_global_cooldown_enabled = true;
       }
-
-      return console.log(rewardData);
 
       let response = await this.rewardService.createReward(rewardData);
 
@@ -85,11 +125,23 @@ export class SongRequestModuleComponent {
         cooldown: response.global_cooldown_seconds
       }
 
+      this.songReward = reward;
+      this.songReward.rewardID = response.rewardID;
+      this.cardAvailable = true;
+
       this.showFormToggle();
       
     }
   }
 
+  deleteReward(): void {
+    let answer = confirm('Are you sure you want to delete this reward?');
+    if (!answer) return;
+    this.rewardService.deleteReward(this.songReward.rewardID);
+    this.showForm = false;
+    this.cardAvailable = false;
+  }
+  
   get _id() { return this.songRequestForm.get('_id'); }
   get title() { return this.songRequestForm.get('title'); }
   get prompt() { return this.songRequestForm.get('prompt'); }
