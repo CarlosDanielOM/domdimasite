117398c3b8c5a1f9595a59b6d36a589d68c7d6b7
CDOM201 <carlos@carlosdaniel.info>
2024-08-11 23:03:46 -0700

    Update links.service.ts

--- src/app/links.service.ts
+++ src/app/links.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LinksService {
-  prod: boolean = false;
+  prod: boolean = true;
   twitchAuthURL: string;
 
   constructor() {
